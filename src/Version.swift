86ab6a3d32c5d1228b7642dfad8312730fa30f64
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-05-28 19:47:53 -0300

    Create struct Version

---
+++ src/Version.swift
@@ -0,0 +1,68 @@
+//
+//  Version.swift
+//  Gollum
+//
+//  Created by eduardo.ferreira on 5/28/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import Foundation
+
+struct Version {
+    let name: String
+    let probability: Float
+}
+
+// MARK: - Extension Equatable
+
+extension Version: Equatable {}
+
+func ==(lhs: Version, rhs: Version) -> Bool {
+    return lhs.name == rhs.name && lhs.probability == rhs.probability
+}
+
+// MARK: - Extension StringLiteralConvertible
+
+extension Version: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+    typealias UnicodeScalarLiteralType = StringLiteralType
+    
+    init(unicodeScalarLiteral value: UnicodeScalarLiteralType) {
+        self.init(value: value)!
+    }
+    
+    init(extendedGraphemeClusterLiteral value: ExtendedGraphemeClusterLiteralType) {
+        self.init(value: value)!
+    }
+    
+    init(stringLiteral value: StringLiteralType) {
+        self.init(value: value)!
+    }
+    
+    // MARK: - Private
+    
+    private init?(value: String) {
+        let fields = Version.extractFieldsFrom(value)
+        
+        if fields.count != 2 {
+            assertionFailure("Syntax error: ABTest case expression must have name and probability values splitted by : (e.g. \"MyTestCaseA:0.5\")")
+            return nil
+        }
+        
+        let name = fields[0]
+        
+        guard let probability = Float(fields[1]) else {
+            assertionFailure("Syntax error: ABTest must have a probablity (e.g. 0.5)")
+            return nil
+        }
+        
+        self.name = name
+        self.probability = probability
+    }
+    
+    // MARK: - Static private
+    
+    static private func extractFieldsFrom(value: String) -> [String] {
+        return value.characters.split { $0 == ":" }.map(String.init)
+    }
+}
---
+++ tests/VersionTest.swift
@@ -0,0 +1,58 @@
+//
+//  VersionTest.swift
+//  Gollum
+//
+//  Created by eduardo.ferreira on 5/28/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import XCTest
+
+class VersionTest: XCTestCase {
+    var version: Version!
+    
+    override func setUp() {
+        super.setUp()
+        
+        version = Version(stringLiteral: "A:0.5")
+    }
+
+    //MARK: - Tests cases
+    
+    func testInitWithStringLiteralTypeValue() {
+        XCTAssertEqual(version.name, "A")
+        XCTAssertEqual(version.probability, 0.5)
+    }
+
+    func testInitWithExtendedGraphemeClusterLiteralTypeValue() {
+        version = Version(extendedGraphemeClusterLiteral: "A:0.4")
+        
+        XCTAssertEqual(version.name, "A")
+        XCTAssertEqual(version.probability, 0.4)
+    }
+    
+    func testInitWithUnicodeScalarLiteralTypeValue() {
+        version = Version(unicodeScalarLiteral: "A:0.3")
+        
+        XCTAssertEqual(version.name, "A")
+        XCTAssertEqual(version.probability, 0.3)
+    }
+    
+    func testEqualableOperator() {
+        let version2 = Version(stringLiteral: "A:0.5")
+        
+        XCTAssert(version == version2)
+    }
+    
+//    func testInitWithoutProbabilityValue() {
+//        version = Version(stringLiteral: "A")
+//    }
+//    
+//    func testInitWithoutNameValue() {
+//        version = Version(stringLiteral: "0.5")
+//    }
+//    
+//    func testInitWithProbabilityNotFloatValue() {
+//        version = Version(stringLiteral: "A:A")
+//    }
+}
