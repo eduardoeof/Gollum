bb375584cfdd496b36bdd8beb6e3763b049644c5
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-05-28 19:04:02 -0300

    Create a demo project to test the framework

---
+++ demo/Gollum/Gollum/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Gollum
+//
+//  Created by eduardo.ferreira on 5/28/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ demo/Gollum/Gollum/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Gollum
+//
+//  Created by eduardo.ferreira on 5/28/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
