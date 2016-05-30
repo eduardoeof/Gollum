9f2230eace5e25e774e35c3005cfe29bd5130c39
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-05-30 09:17:34 -0300

    Create Gollum error enum

---
+++ demo/Gollum/Gollum/GollumError.swift
@@ -0,0 +1,14 @@
+//
+//  GollumError.swift
+//  Gollum
+//
+//  Created by eduardo.ferreira on 5/30/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import Foundation
+
+enum GollumError: ErrorType {
+    case ProbabilitySumIncorrect
+    case TestNotFound
+}
