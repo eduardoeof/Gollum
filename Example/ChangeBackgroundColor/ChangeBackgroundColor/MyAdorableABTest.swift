d6c9e87a21abe92719feba5cc576ecd13c511db4
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-06-06 08:49:30 -0300

    Add ab test with Gollum in example project

---
+++ Example/ChangeBackgroundColor/ChangeBackgroundColor/MyAdorableABTest.swift
@@ -0,0 +1,15 @@
+//
+//  MyAdorableABTest.swift
+//  Gollum
+//
+//  Created by eduardo.ferreira on 5/31/16.
+//  Copyright © 2016 eduardoeof. All rights reserved.
+//
+
+import Foundation
+import Gollum
+
+enum MyAdorableABTest: Version {
+    case A = "A:0.5"
+    case B = "B:0.5"
+}
--- Example/ChangeBackgroundColor/ChangeBackgroundColor/ViewController.swift
+++ Example/ChangeBackgroundColor/ChangeBackgroundColor/ViewController.swift
@@ -7,19 +7,16 @@
 //
 
 import UIKit
+import Gollum
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if Gollum.instance.isVersionSelected(MyAdorableABTest.A) {
+            view.backgroundColor = UIColor.redColor()
+        } else if Gollum.instance.isVersionSelected(MyAdorableABTest.B) {
+            view.backgroundColor = UIColor.greenColor()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
