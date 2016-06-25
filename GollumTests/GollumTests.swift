ae1998aaa37c6a87e6167b9002d1f5dc71b93c35
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-06-25 15:53:08 -0300

    Add @testable on test classes

--- GollumTests/GollumTests.swift
+++ GollumTests/GollumTests.swift
@@ -8,6 +8,8 @@
 
 import XCTest
 
+@testable import Gollum
+
 class GollumTests: XCTestCase {
     var gollum: Gollum!
     
--- GollumTests/VersionUserDefaultDAOTest.swift
+++ GollumTests/VersionUserDefaultDAOTest.swift
@@ -8,6 +8,8 @@
 
 import XCTest
 
+@testable import Gollum
+
 class VersionUserDefaultDAOTest: XCTestCase {
     var dao: VersionUserDefaultDAO!
     
