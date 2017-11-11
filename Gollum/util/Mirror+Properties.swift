4d89287cf6c6099ef258b7bc9fd6d9b0cf0fbc04
Eduardo Ferreira <eduardoofe@gmail.com>
2017-11-10 23:16:50 -0200

    Remove 'characters' deprecated code - In Swift 4 we can use 'count' method directly from a String.

--- Gollum/util/Mirror+Properties.swift
+++ Gollum/util/Mirror+Properties.swift
@@ -12,7 +12,7 @@ extension Mirror {
     var subjectName: String {
         let text = String(describing: self.subjectType)
         let regex = try! NSRegularExpression(pattern: "([a-zA-Z0-9]+)", options: [])
-        let matches = regex.matches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count))
+        let matches = regex.matches(in: text, options: [], range: NSRange(location: 0, length: text.count))
         
         return (text as NSString).substring(with: matches[0].range)
     }
